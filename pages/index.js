98d1edd6347baf2830edc16d0d3193739d8ce05e
Mario Menjivar <mariomenjr@gmail.com>
2019-10-02 22:12:24 -0700

    Added workspace settings for Visual Studio Code

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-export default class Index extends React.Component{
+export default class Index extends React.Component {
     render() {
-        return <h1>Hola Mundo Mundial</h1>
+        return <h1>Hola Mundo Mundial</h1>;
     }
 }
