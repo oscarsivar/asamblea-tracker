3add10ac0ab44e73700000aec804a90ecfca713d
Mario Menjivar <mariomenjr@gmail.com>
2019-10-03 00:19:09 -0700

    Tested implementation of scrappity in an SSR workflow

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
+import diputadosScrapper from "../defaults/diputados.scrapper.json";
+import scrappity from "scrappity";
+
 export default class Index extends React.Component {
+    static async getInitialProps() {
+        const scrapped = await scrappity(diputadosScrapper);
+
+        return { scrapped };
+    }
+
     render() {
+        const congressMembers = this.props.scrapped[0][0];
         return (
-            <button
-                onClick={() => {
-                    const a = 4;
-                    return alert("HolaMundo");
-                }}
-            >
-                Click me!
-            </button>
+            <div>
+                <ul>
+                    {congressMembers.map((member, i) => (
+                        <li key={i}>
+                            <div>{member.props.memberName.value}</div>
+                            <img
+                                src={`${diputadosScrapper.url}${member.props.memberImage.attrs.src}`}
+                                alt={member.props.memberImage.attrs.alt}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            </div>
         );
     }
 }
